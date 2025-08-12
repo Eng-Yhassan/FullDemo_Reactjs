ce394ecd9ccd00a12585569f008134ab248d2351
Eng-Yhassan <muscabhassan81@gmail.com>
2025-08-12 05:39:25 +0300

    Update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from 'react'
 
 export default function Shop() {
   const [xog, setXog] = useState([])
-  const [search, setSearch]= useState("")
-
+  const [search, setSearch] = useState("")
+  const [category, setCategory] = useState("")
 
   const handlXog = () => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
@@ -13,9 +13,12 @@ export default function Shop() {
     }).catch(error => console.log(error));
   }
 
-  const filterXog= xog.filter((xog2)=>{
-    const SearchData= xog2.title.toLowerCase().includes(search.toLowerCase())
-    return SearchData
+  const filterXog = xog.filter((xog2) => {
+    const SearchData =
+      xog2.title.toLowerCase().includes(search.toLowerCase()) ||
+      xog2.category.toLowerCase().includes(search.toLowerCase());
+    const FilterCategory = category ? xog2.category === category : true
+    return SearchData && FilterCategory
   })
 
   useEffect(() => {
@@ -24,23 +27,25 @@ export default function Shop() {
 
   return (<>
     {/* Filter */}
-    <div className='flex gap-20 mx-20 mt-20'>
-      <input value={search} onChange={(e)=>setSearch(e.target.value)} className='w-80 h-10 bg-gray-500 text-white rounded-lg pl-10 outline-none' type="search" placeholder='Search The Product' />
+    <div className='flex gap-20 mx-20 mt-20 '>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} className='w-80 h-10 bg-gray-500 text-white rounded-lg pl-10 outline-none' type="search" placeholder='Search The Product' />
       <div className='text-2xl space-x-10'>
-        <input className='scale-150' type="radio" name='product' /> All
-        <input className='scale-150' type="radio" name='product' /> electronics
-        <input className='scale-150' type="radio" name='product' /> jewelery
-        <input className='scale-150' type="radio" name='product' /> men's clothing
-        <input className='scale-150' type="radio" name='product' /> women's clothing
+        <input onChange={() => setCategory("")} className='scale-150' type="radio" name='product' /> All
+        <input onChange={() => setCategory("electronics")} className='scale-150' type="radio" name='product' /> electronics
+        <input onChange={() => setCategory("jewelery")} className='scale-150' type="radio" name='product' /> jewelery
+        <input onChange={() => setCategory("men's clothing")} className='scale-150' type="radio" name='product' /> men's clothing
+        <input onChange={() => setCategory("women's clothing")} className='scale-150' type="radio" name='product' /> women's clothing
       </div>
     </div>
-    <div className='flex flex-wrap justify-center gap-10 mt-20'>
+    <div className='flex flex-wrap justify-center gap-10 mt-20 mb-20'>
       {
-        filterXog.map((item) => {
+        filterXog.length > 0 ? filterXog.map((item) => {
           return (
             <Cards product={item} />
           )
         })
+          :
+          <p className='text-3xl font-bold'> There no Product</p>
       }
     </div>
   </>
